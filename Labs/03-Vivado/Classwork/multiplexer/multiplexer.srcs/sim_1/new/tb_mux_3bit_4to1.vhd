bf5495a70da968343a9c397069185166d3697fc1
Prophetcze <tomas.kristek56@gmail.com>
2022-02-28 15:19:27 +0100

    multiplexer from vivado update

--- Labs/03-Vivado/Classwork/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
+++ Labs/03-Vivado/Classwork/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -1,5 +1,5 @@
 ------------------------------------------------------------
--- Three-bit wide 4-to-1 multiplexer
+-- Testbench for Three-bit wide 4-to-1 multiplexer
 -- 24.02.2022
 -- Tomas Kristek 230278
 ------------------------------------------------------------
@@ -25,8 +25,8 @@ architecture testbench of tb_mux_3bit_4to1 is
     signal s_b           : std_logic_vector(3 - 1 downto 0);
     signal s_c           : std_logic_vector(3 - 1 downto 0);
     signal s_d           : std_logic_vector(3 - 1 downto 0);
-    signal s_sel_i       : std_logic_vector(2 - 1 downto 0);
-    signal s_f_o         : std_logic_vector(3 - 1 downto 0);
+    signal s_sel       	 : std_logic_vector(2 - 1 downto 0);
+    signal s_f         	 : std_logic_vector(3 - 1 downto 0);
 
 begin
     -- Connecting testbench signals with mux_3bit_4to1
@@ -37,8 +37,8 @@ begin
             b_i      => s_b,
             c_i      => s_c,
             d_i      => s_d,
-            sel_i    => s_sel_i,
-            f_o      => s_f_o
+            sel_i    => s_sel,
+            f_o      => s_f
         );
 
     --------------------------------------------------------
@@ -46,23 +46,35 @@ begin
     --------------------------------------------------------
     p_stimulus : process
     begin
-        -- Report a note at the beginning of stimulus process
-        report "Stimulus process started" severity note;
-        
-        -- Test case
-        s_a <= "110"; 
-        s_sel_i <= "00"; 
+       -- Set one test case and wait 100 ns
+       	s_d		<= "010";
+        s_c 	<= "011"; 
+        s_b 	<= "000"; 
+        s_a 	<= "100";
+        s_sel 	<= "00";
         wait for 100 ns;
         
-        -- Expected output
-        assert ((s_f_o = "110")
+        s_d		<= "011";
+        s_c 	<= "010"; 
+        s_b 	<= "101"; 
+        s_a 	<= "001";
+        s_sel 	<= "01";
+        wait for 100 ns;
         
-        -- If false, then report an error
-        report "Input 110 on a FAILED" severity error;
+        s_d		<= "101";
+        s_c 	<= "111"; 
+        s_b 	<= "110"; 
+        s_a 	<= "010";
+        s_sel	<= "10";
+        wait for 100 ns;
         
-        -- Report a note at the end of stimulus process
-        report "Stimulus process finished" severity note;
-    wait;
+        s_d		<= "100";
+        s_c 	<= "001"; 
+        s_b 	<= "101"; 
+        s_a 	<= "110";
+        s_sel	<= "11";
+        wait for 100 ns;
+        wait;
     end process p_stimulus;
 
 end architecture testbench;
