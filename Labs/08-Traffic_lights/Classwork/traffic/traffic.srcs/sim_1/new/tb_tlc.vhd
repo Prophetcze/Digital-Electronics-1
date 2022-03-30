d45de4621dd978949c1eb72580d58809a7781ee3
Prophetcze <tomas.kristek56@gmail.com>
2022-03-30 16:33:06 +0200

    08-Traffic_lights, Classwork done

--- Labs/08-Traffic_lights/Classwork/traffic/traffic.srcs/sim_1/new/tb_tlc.vhd
+++ Labs/08-Traffic_lights/Classwork/traffic/traffic.srcs/sim_1/new/tb_tlc.vhd
@@ -1,6 +1,6 @@
 ------------------------------------------------------------
 --
--- Template for 4-digit 7-segment display driver testbench.
+-- Traffic lights controller testbench.
 -- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
 --
 -- Copyright (c) 2020-Present Tomas Fryza
@@ -15,14 +15,14 @@ use ieee.std_logic_1164.all;
 ------------------------------------------------------------
 -- Entity declaration for testbench
 ------------------------------------------------------------
-entity tb_driver_7seg_4digits is
+entity tb_tlc is
     -- Entity of testbench is always empty
-end entity tb_driver_7seg_4digits;
+end entity tb_tlc;
 
 ------------------------------------------------------------
 -- Architecture body for testbench
 ------------------------------------------------------------
-architecture testbench of tb_driver_7seg_4digits is
+architecture testbench of tb_tlc is
 
     -- Local constants
     constant c_CLK_100MHZ_PERIOD : time := 10 ns;
@@ -30,30 +30,18 @@ architecture testbench of tb_driver_7seg_4digits is
     --Local signals
     signal s_clk_100MHz : std_logic;
     signal s_reset      : std_logic;
-    signal s_data0      : std_logic_vector(4 - 1 downto 0);
-    signal s_data1      : std_logic_vector(4 - 1 downto 0);
-    signal s_data2      : std_logic_vector(4 - 1 downto 0);
-    signal s_data3      : std_logic_vector(4 - 1 downto 0);
-    signal s_dpin       : std_logic_vector(4 - 1 downto 0);
-    signal s_dpout      : std_logic;
-    signal s_seg        : std_logic_vector(7 - 1 downto 0);
-    signal s_dig        : std_logic_vector(4 - 1 downto 0);
+    signal s_south      : std_logic_vector(3 - 1 downto 0);
+    signal s_west       : std_logic_vector(3 - 1 downto 0);
 
 begin
-    -- Connecting testbench signals with driver_7seg_4digits
-    -- entity (Unit Under Test)
-    uut_dri : entity work.driver_7seg_4digits
+    -- Connecting testbench signals with tlc entity
+    -- (Unit Under Test)
+    uut_tlc : entity work.tlc
         port map(
-            clk      => s_clk_100MHz,
-            reset    => s_reset,
-            data0_i  => s_data0,
-            data1_i  => s_data1,
-            data2_i  => s_data2,
-            data3_i  => s_data3,
-            dp_i     => s_dpin,
-            dp_o     => s_dpout,
-            seg_o    => s_seg,
-            dig_o    => s_dig
+            clk     => s_clk_100MHz,
+            reset   => s_reset,
+            south_o => s_south,
+            west_o  => s_west
         );
 
     --------------------------------------------------------
@@ -61,7 +49,7 @@ begin
     --------------------------------------------------------
     p_clk_gen : process
     begin
-        while now < 750 ns loop -- 75 periods of 100MHz clock
+        while now < 10000 ns loop -- 10 usec of simulation
             s_clk_100MHz <= '0';
             wait for c_CLK_100MHZ_PERIOD / 2;
             s_clk_100MHz <= '1';
@@ -75,13 +63,13 @@ begin
     --------------------------------------------------------
     p_reset_gen : process
     begin
-        s_reset <= '0'; wait for 12 ns;
+        s_reset <= '0'; wait for 200 ns;
         -- Reset activated
-        s_reset <= '1'; wait for 86 ns;
+        s_reset <= '1'; wait for 500 ns;
         -- Reset deactivated
-        s_reset <= '0'; wait for 456 ns;
+        s_reset <= '0'; wait for 5000 ns;
         -- Reset activated
-        s_reset <= '1'; wait for 54 ns;
+        s_reset <= '1'; wait for 1000 ns;
         -- Reset deactivated
         s_reset <= '0';
         wait;
@@ -92,16 +80,7 @@ begin
     --------------------------------------------------------
     p_stimulus : process
     begin
-        report "Stimulus process started" severity note;
-        
-        -- set input data as 3.142
-        s_data3 <= "0011"; -- 3
-        s_data2 <= "0001"; -- 1
-        s_data1 <= "0100"; -- 4
-        s_data0 <= "0010"; -- 2
-        s_dpin  <= "0111"; -- decimal point
-        
-        report "Stimulus process finished" severity note;
+        -- No input data needed.
         wait;
     end process p_stimulus;
 
